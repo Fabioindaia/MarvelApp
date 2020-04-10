091464fa41f26afb27f5926aace498967e46f5d6
Fábio Flores <fabiocrflores@gmail.com>
2020-04-10 17:23:40 -0300

    - Alterado arquitetura para MVVM; - Implementado as libs do RxJava, LiveData, Picasso e Paging - Criado classe para arquivar as chaves do usuário da Marvel; - Alterado arquivo .gitignore; - Alterado arquivo README;

--- app/src/main/java/com/uolinc/marvelapp/util/Keys.kt
+++
@@ -1,7 +0,0 @@
-package com.uolinc.marvelapp.util
-
-object Keys {
-
-    const val privateKey = "8bf03c0132a99e81383dab2298061c82e86c0d94"
-    const val apiKey = "f4c8f084c2f5a73f09ae3c2ecff2be9f"
-}
